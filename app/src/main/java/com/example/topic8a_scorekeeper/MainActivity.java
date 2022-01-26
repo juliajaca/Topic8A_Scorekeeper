e128dfb602f2c570be92fb01cdce485f5ffd1f0c
Julia Jaca <jjaca@brujula.es>
2022-01-26 14:45:30 +0100

    todo menos el challenge

--- app/src/main/java/com/example/topic8a_scorekeeper/MainActivity.java
+++ app/src/main/java/com/example/topic8a_scorekeeper/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.topic8a_scorekeeper;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static  final String STATE_SCORE_1  = "puntuacion1";
+    private static final String STATE_SCORE_2 = "puntuacion2" ;
     private TextView puntuacion1Vista;
     private  TextView puntuacion2Vista;
     private ImageView botonMas1, botonMenos1, botonMas2, botonMenos2;
@@ -51,6 +56,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        if (savedInstanceState != null) {
+            puntuacion1 = savedInstanceState.getInt(STATE_SCORE_1);
+            puntuacion2 = savedInstanceState.getInt(STATE_SCORE_2);
+
+            //Set the score text views
+            puntuacion1Vista.setText(String.valueOf(puntuacion1));
+            puntuacion2Vista.setText(String.valueOf(puntuacion2));
+        }
     }
 
     private void increaseScore(View v){
@@ -86,6 +99,43 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Inflate the menu from XML
+        getMenuInflater().inflate(R.menu.main_menu, menu);
 
+        //Change the label of the menu based on the state of the app
+        int nightMode = AppCompatDelegate.getDefaultNightMode();
+        if(nightMode == AppCompatDelegate.MODE_NIGHT_YES){
 
+            menu.findItem(R.id.night_mode).setTitle(R.string.day_mode);
+        } else{
+            menu.findItem(R.id.night_mode).setTitle(R.string.night_mode);
+        }
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //Check if the correct item was clicked
+        if(item.getItemId()==R.id.night_mode){
+            //Get the night mode state of the app
+            int nightMode = AppCompatDelegate.getDefaultNightMode();
+        //Set the theme mode for the restarted activity
+            if(nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            }else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            }
+        }
+        recreate();
+        return true;
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        //Save the scores
+        outState.putInt(STATE_SCORE_1, puntuacion1);
+        outState.putInt(STATE_SCORE_2, puntuacion2);
+        super.onSaveInstanceState(outState);
+    }
 }
